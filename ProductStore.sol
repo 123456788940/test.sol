73ed3fab3b5c119d1b7f69b4ef70d64ea30da93a
Soham Dutta <123827172+123456788940@users.noreply.github.com>
2023-12-13 12:34:14 +0530

    Update ProductStore.sol
    // SPDX-License-Identifier: MIT
    pragma solidity ^0.8;
    Declares the license
    And the compiler version
    import "./erc20.sol";
    
    Imports the former erc20 contract to this
    struct product {
            string _type;
            uint quantity;
            bool added;
        }
    
    
    Structures the product with the variables underneath.
     mapping(address => product) public Product;
    Maps the product structure
    
    
        constructor(address _tokenAddress) {
         token = erc20(_tokenAddress);
         
        }
    constructor executed once while declaration initializes the token erc20
        function addProduct(string memory _type, uint quantity) public {
            require(!Product[msg.sender].added, "not sold");
    
    
            Product[msg.sender] = product ({
                _type: _type,
                quantity: quantity,
                added: true
    
    
            });
    
    
        }
    Adds the product to be purchased
    
    
        function buyProduct() public {
            require(Product[msg.sender].added, "not sold");
                require(Product[msg.sender].quantity > 0, "has to be more than 100");
                Product[msg.sender].quantity--;
                   
               
     
             token.transferFrom(msg.sender, address(this), 100);
       
    
    
    
    
        }
    
    
    }
    Executes the purchase by sending the money

--- ProductStore.sol
+++ ProductStore.sol
@@ -1,4 +1,39 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "./erc20.sol";
+
+contract productStore {
+    erc20 public token;
+   
+
+    struct product {
+        string _type;
+        uint quantity;
+        bool added;
+    }
+
+    mapping(address => product) public Product;
+
+    constructor(address _tokenAddress) {
+     token = erc20(_tokenAddress);
+     
+    }
+
+    function addProduct(string memory _type, uint quantity) public {
+        require(!Product[msg.sender].added, "not sold");
+
+        Product[msg.sender] = product ({
+            _type: _type,
+            quantity: quantity,
+            added: true
+
+        });
+
+    }
+
+
     function buyProduct() public {
         require(Product[msg.sender].added, "not sold");
             require(Product[msg.sender].quantity > 0, "has to be more than 100");
